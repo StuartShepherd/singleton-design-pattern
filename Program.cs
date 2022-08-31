f60a1e7dc1b75dd0a3ebaf62f73ab6ace8bc7c02
stuartshepherd@gmail.com <stuartshepherd@gmail.com>
2022-08-31 16:11:32 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using SingletonDesignPattern;
+
+Console.WriteLine("Singleton Design Pattern");
+Console.WriteLine("Singleton is a creational design pattern, which ensures that only one object of its kind exists and provides a single point of access to it for any other code.");
+Console.WriteLine();
+
+var singleton1 = Singleton.GetInstance();
+var singleton2 = Singleton.GetInstance();
+
+if (singleton1 == singleton2)
+    Console.WriteLine("Both variables contain the same instance.");
+
+if (singleton1 != singleton2)
+    Console.WriteLine("Variables contain different instances.");
